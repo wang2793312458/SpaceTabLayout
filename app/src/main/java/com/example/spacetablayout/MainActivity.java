922103d3a816669de0fe4a01a9c365565d5f0f36
wz2793312458 <wz2793312458@163.com>
2016-12-23 11:50:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/spacetablayout/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.spacetablayout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.spacetablayout2.SpaceTabLayout;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    SpaceTabLayout tabLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //add the fragments you want to display in a List
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new FragmentA());
+        fragmentList.add(new FragmentB());
+        fragmentList.add(new FragmentC());
+        fragmentList.add(new FragmentD());
+        fragmentList.add(new FragmentE());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (SpaceTabLayout) findViewById(R.id.spaceTabLayout);
+
+        //we need the savedInstanceState to retrieve the position
+        tabLayout.initialize(viewPager, getSupportFragmentManager(), fragmentList, savedInstanceState);
+    }
+
+
+    //we need the outState to memorize the position
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        tabLayout.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+}
